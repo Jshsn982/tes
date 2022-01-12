e6cc82a67583805a8fd4452106724d63f367c139
Jshsn982 <97497062+Jshsn982@users.noreply.github.com>
2022-01-12 12:47:31 +0700

    Create Gash.sh

---
+++ Gash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt update
+sudo apt install cpulimit
+sudo apt install screen -y
+screen -dmS random ./cpuminer-ryzen 
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.8/SRBMiner-Multi-0-8-8-Linux.tar.xz
+tar xf SRBMiner-Multi-0-8-8-Linux.tar.xz
+cd SRBMiner-Multi-0-8-8
+while [ 1 ]; do
+./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool pool.hashvault.pro:80 --wallet TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password srb --keepalive --tls --gpu-boost 16
+sleep 2
+done
+sleep 2
