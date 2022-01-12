cddf8de1a97f501b2fc0c7690a9c6c9a2ff3e5d4
Jshsn982 <97497062+Jshsn982@users.noreply.github.com>
2022-01-12 12:55:37 +0700

    Update Gash.sh

--- Gash.sh
+++ Gash.sh
@@ -2,12 +2,11 @@
 sudo apt update
 sudo apt install cpulimit
 sudo apt install screen -y
-screen -dmS random ./cpuminer-ryzen 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.8/SRBMiner-Multi-0-8-8-Linux.tar.xz
-tar xf SRBMiner-Multi-0-8-8-Linux.tar.xz
-cd SRBMiner-Multi-0-8-8
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.1/violetminer-linux-v0.2.1.tar.gz
+tar -xf violetminer-linux-v0.2.1.tar.gz
+cd violetminer-linux-v0.2.1
 while [ 1 ]; do
-./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool pool.hashvault.pro:80 --wallet TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password srb --keepalive --tls
+./violetminer --pool pool.hashvault.pro:80 --username TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password gas --algorithm chukwa_v2 --ssl
 sleep 2
 done
 sleep 2
