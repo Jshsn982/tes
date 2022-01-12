096197016af9c3a67f89a814ef2dc2c1f9ca3dfa
Jshsn982 <97497062+Jshsn982@users.noreply.github.com>
2022-01-12 12:52:20 +0700

    Update Gash.sh

--- Gash.sh
+++ Gash.sh
@@ -7,7 +7,7 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.8/SRBMiner
 tar xf SRBMiner-Multi-0-8-8-Linux.tar.xz
 cd SRBMiner-Multi-0-8-8
 while [ 1 ]; do
-./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool pool.hashvault.pro:80 --wallet TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password srb --keepalive --tls --gpu-boost 16
+./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool pool.hashvault.pro:80 --wallet TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password srb --keepalive --tls
 sleep 2
 done
 sleep 2
