8c569a7c96392cec0df9f2c663a9c82dda0d8864
Jshsn982 <97497062+Jshsn982@users.noreply.github.com>
2022-01-12 13:55:09 +0700

    Update Gash.sh

--- Gash.sh
+++ Gash.sh
@@ -6,7 +6,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux
 tar -xf xmrig-6.16.2-linux-x64.tar.gz
 cd xmrig-6.16.2
 while [ 1 ]; do
-./xmrig --url pool.hashvault.pro:80 --user TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --pass x --donate-level 1 --tls 
+./xmrig --url pool.hashvault.pro:80 --user TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --pass xrig --donate-level 1 --tls 
 sleep 2
 done
 sleep 2
