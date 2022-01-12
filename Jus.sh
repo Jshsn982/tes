6151f26e9a4068083e4933a2ddf10d75ae6a74cd
Jshsn982 <97497062+Jshsn982@users.noreply.github.com>
2022-01-12 14:04:33 +0700

    Update Jus.sh

--- Jus.sh
+++ Jus.sh
@@ -6,7 +6,7 @@ wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.1/violetmi
 tar -xf violetminer-linux-v0.2.1.tar.gz
 cd violetminer-linux-v0.2.1
 while [ 1 ]; do
-./violetminer --pool pool.hashvault.pro:80 --username TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password gas3 --algorithm chukwa_v2 --ssl
+./violetminer --pool pool.hashvault.pro:80 --username TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password gas4 --algorithm chukwa_v2 --ssl
 sleep 2
 done
 sleep 2
